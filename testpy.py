70766d5fe5df34fb46d8023d9cab0579b6014750
MrTorstein <54394656+MrTorstein@users.noreply.github.com>
2019-08-23 10:21:12 +0200

    Create testpy.py

---
+++ testpy.py
@@ -0,0 +1,12 @@
+coding=utf-8
+
+from numpy import *
+
+class Noe():
+  __init__(self, kalle):
+    kalle = kalle
+    bjarne = "Bjarne"
+
+if __name__ == "__main__":
+  A = Noe("Kalle")
+  print(A.kalle, A.bjarne)
